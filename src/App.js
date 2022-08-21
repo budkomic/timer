6849d263bd286be99ba8a63e7dc123fc68b9d9c1
Michal Budkowski <michalbudkowski88@gmail.com>
2022-08-21 17:36:26 +0200

    timer add

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import TimeBox from './components/TimeBox/TimeBox';
+import Button from './components/Button/Button';
+import Container from './components/Container/Container';
+import { useState, useEffect } from 'react';
+
+
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue + 1);
+    }, 1))
+  };
+
+  const stop = () => {
+    clearInterval(timer)
+  };
+
+  const reset = () => {
+    setTime(0);
+    setTimer(null);
+  };
+
+  useEffect(() => {
+    return () => {
+       if(timer) {clearInterval(timer)};
+      };
+  }, []);
+
+  return (
+    <Container>
+      <TimeBox>{time}</TimeBox>
+      <Button value={start}>START</Button>
+      <Button value={stop}>STOP</Button>
+      <Button value={reset}>RESET</Button>
+    </Container>
+  );
+};
+
+export default App;
+test  
